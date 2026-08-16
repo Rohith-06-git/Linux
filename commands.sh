ee9e82a850388bdfc55d771b049663be03c8bd36
Rohith Chandra <rohithchandra12345@gmail.com>
2026-08-16 22:17:36 +0530

    Learned commands

--- commands.sh
+++ commands.sh
@@ -1,7 +1,27 @@
+# Linux Basics
+
+# Navigation
 pwd
 ls
 cd ..
+
+# Create and remove directories/files
 mkdir test
 touch file.txt
 rm file.txt
 rmdir test
+
+# File operations
+touch file1.txt
+echo "Hello Linux" > file1.txt
+cat file1.txt
+
+cp file1.txt file2.txt
+mv file2.txt renamed.txt
+
+echo "Second line" >> renamed.txt
+
+head renamed.txt
+tail renamed.txt
+
+rm file1.txt renamed.txt
